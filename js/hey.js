b2647a11aba3eec08d343667642b5ba802095b19
sanechkae <72148463+sanechkae@users.noreply.github.com>
2021-03-08 21:03:15 +0300

    Delete hey.js

--- js/hey.js
+++
@@ -1,9 +0,0 @@
-function clickMe() {
-    var text = document.querySelector('#text').value;
-    var output = "";
-    for ( var letter of text){
-        output = output + letter;
-        console.log (output);
-
-    }
-}
